c55bbb2f78345f84bc804fcacc1565cd5f6c4151
Steven Garcia <stevgarcia2015@gmail.com>
2021-07-13 09:23:31 -0700

    Add to skills section

--- src/data/skills.data.js
+++ src/data/skills.data.js
@@ -1,16 +1,24 @@
 const Skills = [
     'JavaScript',
+    'TypeScript',
     'React',
     'Redux',
     'Node.js',
+    'Express.js',
     'MySQL',
+    'Mongoose/MongoDb',
     'HTML5',
     'CSS3',
     'ASP.NET Core',
+    'Entity Framework Core',
     'C#',
     'Git',
     'Java',
-    'Spring Boot'
+    'Spring Boot',
+    'Spring Security',
+    'Spring Data JPA',
+    'Docker',
+    'AWS (S3, EC2, RDS, etc.)'
 ];
 
 export default Skills;
